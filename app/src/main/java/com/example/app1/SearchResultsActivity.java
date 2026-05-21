38ff14ed18d9be2581e3ffc99616d517a7ab6b5d
Mohammad <m.abakhr@gmail.com>
2026-05-20 23:23:52 -0700

    Change SearchResultsActivity

--- app/src/main/java/com/example/app1/SearchResultsActivity.java
+++ app/src/main/java/com/example/app1/SearchResultsActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
@@ -42,31 +44,51 @@ public class SearchResultsActivity extends AppCompatActivity {
     }
 
     private void loadJobs() {
-
-        FBRef.refPosts.addListenerForSingleValueEvent(new ValueEventListener() {
-
+        FBRef.refPosts.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                list.clear();
-
-                for (DataSnapshot data : snapshot.getChildren()) {
-
-                    JobPost job = data.getValue(JobPost.class);
-
-                    if (job != null) {
-
-                        list.add(job);
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if( task.isSuccessful()){
+                    list.clear();
+                    DataSnapshot ds = task.getResult();
+                    for (DataSnapshot userPosts : ds.getChildren()) {
+                        for( DataSnapshot posts : userPosts.getChildren()){
+                            JobPost job = posts.getValue(JobPost.class);
+                            list.add(job);
+                        }
                     }
+                    adapter.notifyDataSetChanged();
+
                 }
 
-                adapter.notifyDataSetChanged();
             }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
         });
+
+
+
+
+
+//        FBRef.refPosts.addListenerForSingleValueEvent(new ValueEventListener() {
+//
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                list.clear();
+//
+//                for (DataSnapshot data : snapshot.getChildren()) {
+//
+//                    JobPost job = data.getValue(JobPost.class);
+//
+//                    if (job != null) {
+//
+//                        list.add(job);
+//                    }
+//                }
+//
+//                adapter.notifyDataSetChanged();
+//            }
+
+
+ //       });
     }
 }
